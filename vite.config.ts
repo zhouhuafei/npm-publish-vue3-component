64668d9694f36a0b844c4600f024234db548a707
zhouhuafei <1123486116@qq.com>
2022-04-20 19:33:57 +0800

    fix: 🐛 修复单元测试 - 使之能够识别TSX组件

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,11 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  test: {
+    transformMode: {
+      web: [/\.[jt]sx$/]
+    }
+  },
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
