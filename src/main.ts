c7236f485ce8927d60f96e4945b0dee63095f8e6
zhouhuafei <1123486116@qq.com>
2026-01-26 10:30:16 +0800

    feat: 🎸 引入css

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import {
-  V3Button1,
-  V3Button2
-} from '../lib-src'
+// 开发阶段 - 调试 - 会自动引入css
+// import { V3Button1, V3Button2 } from '../lib-src'
+// 生产阶段 - 调试 - 需手动引入css
+import '../lib/index.css'
+import { V3Button1, V3Button2 } from '../lib/index.es.js'
 
 const instance = createApp(App)
 instance.use(V3Button1)
