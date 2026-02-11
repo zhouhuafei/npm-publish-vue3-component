44b7214b3f699608254fe376ace8ee3360800f03
zhouhuafei <1123486116@qq.com>
2026-02-11 14:43:50 +0800

    feat: 🎸 HelloWorld2

--- src/components/HelloWorld2/index.tsx
+++ src/components/HelloWorld2/index.tsx
@@ -1,21 +1,10 @@
-// ********************************* import
 import './index.global.scss'
 import css from './index.module.scss'
-import { defineOptions, defineProps } from 'vue'
-// ********************************* defineOptions
-defineOptions({ name: 'HelloWorld2' })
-// ********************************* const and let
-// ********************************* props and emits
-const props = defineProps({
-  msg: {
-    type: String,
-    default: ''
-  }
-})
-// ********************************* thisFields and thisMethods and lifetimes
-// ********************************* otherFields and otherMethods
-// ********************************* computedFields and watch
-// ********************************* defineExpose
-export default () => {
+
+const HelloWorld2 = (props = { msg: '' }) => {
   return <div class={`HelloWorld2 ${css.HelloWorld2}`}>{props.msg}</div>
 }
+
+HelloWorld2.displayName = 'HelloWorld2'
+
+export default HelloWorld2
