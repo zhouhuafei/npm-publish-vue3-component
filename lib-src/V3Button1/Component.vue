40efc5c1e9cdae3f041ef2a87f2dcfeb2d97917a
zhouhuafei <1123486116@qq.com>
2026-02-11 13:03:54 +0800

    feat: 🎸 V3Button1

--- lib-src/V3Button1/Component.vue
+++ lib-src/V3Button1/Component.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="v3Button1">
-    {{ msg }}
+    {{ props.msg }}
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-  name: 'V3Button1',
-  props: {
-    msg: String
+<script lang="ts" setup>
+// ********************************* import
+// ********************************* defineOptions
+defineOptions({ name: 'V3Button1' })
+// ********************************* const and let
+// ********************************* props and emits
+const props = defineProps({
+  msg: {
+    type: String,
+    default: ''
   }
 })
+// ********************************* thisFields and thisMethods and lifetimes
+// ********************************* otherFields and otherMethods
+// ********************************* computedFields and watch
+// ********************************* defineExpose
+// ********************************* defineOptions
 </script>
 
 <style scoped lang="scss">
